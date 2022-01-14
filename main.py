3e5f14b0b3c0c33406b72eeec6715e388e502c22
Giovanni Valdigem <gsvaldigem@gmail.com>
2022-01-14 07:55:36 -0300

    primeira pag
    primeira parte da primeira pagina

---
+++ main.py
@@ -0,0 +1,16 @@
+
+from flask import render_template, Flask
+import os
+
+app = Flask(__name__)
+
+if __name__== 'main':
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port = port)
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
