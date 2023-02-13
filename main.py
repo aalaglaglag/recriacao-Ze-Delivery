d1f4d77bbbaea0f57d410d1c2763c5c9ece741ac
Giovanni Valdigem <69513026+aalaglaglag@users.noreply.github.com>
2023-02-13 14:44:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import os
 
 app = Flask(__name__)
 
+if __name__== 'main':
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port = port)
+    
 
-@app.route("aalaglaglag/recriacao-Ze-Delivery/settings/pages", methods=['GET', 'POST'])
 def home():
-    return render_template("templates\index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
