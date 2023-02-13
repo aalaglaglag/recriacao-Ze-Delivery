1f2714c8f2d86cd55715516c46858b171d5878d1
Giovanni Valdigem <69513026+aalaglaglag@users.noreply.github.com>
2023-02-13 14:43:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("aalaglaglag/recriacao-Ze-Delivery/settings/pages", methods=['GET', 'POST'])
 def home():
-    return render_template("index.html")
+    return render_template("templates\index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
