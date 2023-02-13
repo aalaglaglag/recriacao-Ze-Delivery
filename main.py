9b6ab674a5343aa0c6964d4e6f43cccfbce3ab58
Giovanni Valdigem <69513026+aalaglaglag@users.noreply.github.com>
2023-02-13 14:41:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,8 @@ import os
 
 app = Flask(__name__)
 
-if __name__== 'main':
-    port = int(os.getenv('PORT'), '5000')
-    app.run(host='0.0.0.0', port = port)
 
-@app.route("https://github.com/aalaglaglag/recriacao-Ze-Delivery/settings/pages", methods=['GET', 'POST'])
+@app.route("aalaglaglag/recriacao-Ze-Delivery/settings/pages", methods=['GET', 'POST'])
 def home():
     return render_template("index.html")
 
